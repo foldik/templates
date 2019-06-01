81a947c8811688e78eb15033a3b7da8053d241a4
foldik <foldi.kristof@gmail.com>
2019-06-01 15:01:34 +0200

    Polished pagination buttons

--- elm-spa/src/Components/PaginatedCardList.elm
+++ elm-spa/src/Components/PaginatedCardList.elm
@@ -29,7 +29,7 @@ view : Model a msg -> Html msg
 view model =
     div []
         [ pagingView model
-        , cardTable 4 model.items model.itemView
+        , cardTable 3 model.items model.itemView
         , pagingView model
         ]
 
@@ -45,22 +45,6 @@ pagingView pagination =
 
     else
         let
-            previousButton =
-                if [ pagination.count <= 5, pagination.page == 1, pagination.count == 0 ] |> List.any (\n -> n == True) then
-                    span [] []
-
-                else
-                    a [ href (pagination.hrefFunc (pagination.page - 1) pagination.limit), class "pagination-previous button" ]
-                        [ text "Previous" ]
-
-            nextButton =
-                if [ pagination.count <= 5, pagination.page == pagination.count ] |> List.any (\n -> n == True) then
-                    span [] []
-
-                else
-                    a [ href (pagination.hrefFunc (pagination.page + 1) pagination.limit), class "pagination-next button" ]
-                        [ text "Next page" ]
-
             pageButtons =
                 pageButtonsView pagination
         in
@@ -69,9 +53,7 @@ pagingView pagination =
             , attribute "role" "navigation"
             , attribute "aria-label" "pagination"
             ]
-            [ previousButton
-            , nextButton
-            , pageButtons
+            [ pageButtons
             ]
 
 
@@ -86,30 +68,65 @@ pageButtonsView pagination =
 type PaginationButton
     = Number Int
     | Dots
+    | Previous
+    | Next
 
 
 pageButtonsList : Model a msg -> List PaginationButton
 pageButtonsList pagination =
-    if pagination.count < 1 then
-        []
+    let
+        buttons =
+            if pagination.count < 1 then
+                []
 
-    else if pagination.count <= 5 then
-        List.range 1 pagination.count
-            |> List.map (\n -> Number n)
+            else if pagination.count <= 5 then
+                List.range 1 pagination.count
+                    |> List.map (\n -> Number n)
 
-    else if pagination.page < 4 then
-        [ Number 1, Number 2, Number 3, Number 4, Dots, Number pagination.count ]
+            else if pagination.page < 4 then
+                [ Number 1, Number 2, Number 3, Number 4, Dots, Number pagination.count ]
 
-    else if pagination.page > (pagination.count - 3) then
-        [ Number 1, Dots, Number (pagination.count - 3), Number (pagination.count - 2), Number (pagination.count - 1), Number pagination.count ]
+            else if pagination.page > (pagination.count - 3) then
+                [ Number 1, Dots, Number (pagination.count - 3), Number (pagination.count - 2), Number (pagination.count - 1), Number pagination.count ]
 
-    else
-        [ Number 1, Dots, Number (pagination.page - 1), Number pagination.page, Number (pagination.page + 1), Dots, Number pagination.count ]
+            else
+                [ Number 1, Dots, Number (pagination.page - 1), Number pagination.page, Number (pagination.page + 1), Dots, Number pagination.count ]
+    in
+    case ( pagination.page > 1, pagination.page < pagination.count ) of
+        ( True, True ) ->
+            (Previous :: buttons) ++ [ Next ]
+
+        ( True, False ) ->
+            Previous :: buttons
+
+        ( False, True ) ->
+            buttons ++ [ Next ]
+
+        ( False, False ) ->
+            buttons
 
 
 pageButtonView : PaginationButton -> Model a msg -> Html msg
 pageButtonView paginationButton pagination =
     case paginationButton of
+        Previous ->
+            li []
+                [ a
+                    [ href (pagination.hrefFunc (pagination.page - 1) pagination.limit)
+                    , class "pagination-link button"
+                    ]
+                    [ text (String.fromChar '←') ]
+                ]
+
+        Next ->
+            li []
+                [ a
+                    [ href (pagination.hrefFunc (pagination.page + 1) pagination.limit)
+                    , class "pagination-link button"
+                    ]
+                    [ text (String.fromChar '→') ]
+                ]
+
         Dots ->
             li []
                 [ span [ class "pagination-ellipsis" ]
