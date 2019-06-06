7f8ae10d1a7f82dc6316490cd8bfc969555ecf4d
foldik <foldi.kristof@gmail.com>
2019-06-06 23:07:29 +0200

    Polish form page

--- elm-spa/src/Form.elm
+++ elm-spa/src/Form.elm
@@ -18,13 +18,82 @@ import User
 
 type alias Model =
     { session : Session.Session
-    , serviceName : String
+    , serviceName : FormValue String
     }
 
 
 init : Session.Session -> ( Model, Cmd Msg )
 init session =
-    ( Model session "", Cmd.none )
+    ( Model session (formParam "" [ notEmpty ]), Cmd.none )
+
+
+
+-- Form / Validation
+
+
+type alias FormValue a =
+    ( a, List (a -> ValidationResult), ValidationResult )
+
+
+formParam : a -> List (a -> ValidationResult) -> FormValue a
+formParam value validations =
+    ( value, validations, Valid )
+
+
+getValue : FormValue a -> a
+getValue ( value, _, _ ) =
+    value
+
+
+updateValue : a -> FormValue a -> FormValue a
+updateValue value ( _, validations, validationResult ) =
+    ( value, validations, Valid )
+
+
+validate : FormValue a -> FormValue a
+validate ( value, validations, _ ) =
+    let
+        validationResult =
+            List.map (\rule -> rule value) validations
+                |> List.map
+                    (\result ->
+                        case result of
+                            Invalid reason ->
+                                reason
+
+                            Valid ->
+                                ""
+                    )
+                |> List.filter
+                    (\message ->
+                        case message of
+                            "" ->
+                                False
+
+                            innerMessage ->
+                                True
+                    )
+    in
+    if List.isEmpty validationResult then
+        ( value, validations, Valid )
+
+    else
+        ( value, validations, Invalid (String.join "" validationResult) )
+
+
+notEmpty : String -> ValidationResult
+notEmpty =
+    \value ->
+        if String.length value == 0 then
+            Invalid "Must not be empty"
+
+        else
+            Valid
+
+
+type ValidationResult
+    = Valid
+    | Invalid String
 
 
 
@@ -33,13 +102,26 @@ init session =
 
 type Msg
     = UpdateServiceName String
+    | Submit
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         UpdateServiceName value ->
-            ( { model | serviceName = value }, Cmd.none )
+            ( { model | serviceName = updateValue value model.serviceName }, Cmd.none )
+
+        Submit ->
+            let
+                serviceName =
+                    validate model.serviceName
+            in
+            case serviceName of
+                ( _, _, Invalid reason ) ->
+                    ( { model | serviceName = serviceName }, Cmd.none )
+
+                ( _, _, Valid ) ->
+                    ( { model | serviceName = formParam "" [ notEmpty ] }, Cmd.none )
 
 
 
@@ -59,8 +141,13 @@ view model =
                         [ text "Service name:" ]
                     ]
                 , div []
-                    [ input [ type_ "text", placeholder "Service name", value model.serviceName, onInput UpdateServiceName ]
+                    [ input [ type_ "text", placeholder "Service name", value (getValue model.serviceName), onInput UpdateServiceName, autofocus True ]
                         []
+                    , validationView model.serviceName
+                    ]
+                , div []
+                    [ button [ onClick Submit, type_ "button" ]
+                        [ text "Save" ]
                     ]
                 ]
             ]
@@ -73,5 +160,16 @@ modelView model =
         [ h1 []
             [ text "Model" ]
         , p []
-            [ text ("serviceName: " ++ model.serviceName) ]
+            [ text ("serviceName: " ++ getValue model.serviceName) ]
         ]
+
+
+validationView : FormValue a -> Html Msg
+validationView ( _, _, validationResult ) =
+    case validationResult of
+        Valid ->
+            span [] []
+
+        Invalid reason ->
+            p []
+                [ text reason ]
