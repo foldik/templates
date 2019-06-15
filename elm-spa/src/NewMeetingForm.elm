27347a1c3a428d77aa87273c507b2a7959de2b98
foldik <foldi.kristof@gmail.com>
2019-06-15 19:26:44 +0200

    Add form elements

--- elm-spa/src/NewMeetingForm.elm
+++ elm-spa/src/NewMeetingForm.elm
@@ -20,12 +20,34 @@ import User
 
 type alias Model =
     { session : Session.Session
+    , newMeeting : NewMeeting
+    }
+
+
+type alias NewMeeting =
+    { title : Forms.Value String
+    , description : Forms.Value String
+    , date : Forms.Value String
+    , from : Forms.Value String
+    , to : Forms.Value String
+    , participiants : List (Forms.Value String)
     }
 
 
 init : Session.Session -> ( Model, Cmd Msg )
 init session =
-    ( Model session, Cmd.none )
+    ( Model session emptyNewMeeting, Cmd.none )
+
+
+emptyNewMeeting : NewMeeting
+emptyNewMeeting =
+    { title = Forms.formParam "" [ Forms.notEmpty ]
+    , description = Forms.formParam "" [ Forms.notEmpty ]
+    , date = Forms.formParam "" [ Forms.notEmpty ]
+    , from = Forms.formParam "" [ Forms.notEmpty ]
+    , to = Forms.formParam "" [ Forms.notEmpty ]
+    , participiants = []
+    }
 
 
 
@@ -33,14 +55,65 @@ init session =
 
 
 type Msg
-    = NoOp
+    = UpdateTitle String
+    | UpdateDescription String
+    | UpdateDate String
+    | UpdateFrom String
+    | UpdateTo String
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        UpdateTitle value ->
+            let
+                meeting =
+                    model.newMeeting
+
+                newMeeting =
+                    { meeting | title = Forms.setValue value meeting.title }
+            in
+            ( { model | newMeeting = newMeeting }, Cmd.none )
+
+        UpdateDescription value ->
+            let
+                meeting =
+                    model.newMeeting
+
+                newMeeting =
+                    { meeting | description = Forms.setValue value meeting.description }
+            in
+            ( { model | newMeeting = newMeeting }, Cmd.none )
+
+        UpdateDate value ->
+            let
+                meeting =
+                    model.newMeeting
+
+                newMeeting =
+                    { meeting | date = Forms.setValue value meeting.date }
+            in
+            ( { model | newMeeting = newMeeting }, Cmd.none )
+
+        UpdateFrom value ->
+            let
+                meeting =
+                    model.newMeeting
+
+                newMeeting =
+                    { meeting | from = Forms.setValue value meeting.from }
+            in
+            ( { model | newMeeting = newMeeting }, Cmd.none )
+
+        UpdateTo value ->
+            let
+                meeting =
+                    model.newMeeting
+
+                newMeeting =
+                    { meeting | to = Forms.setValue value meeting.to }
+            in
+            ( { model | newMeeting = newMeeting }, Cmd.none )
 
 
 
@@ -49,5 +122,37 @@ update msg model =
 
 view : Model -> Html Msg
 view model =
-    div []
-        []
+    Html.form []
+        [ fieldset []
+            [ legend []
+                [ text "New Meeting" ]
+            , div []
+                [ label []
+                    [ text "Title:" ]
+                ]
+            , div []
+                [ input [ type_ "text", placeholder "Title", onInput UpdateTitle ]
+                    []
+                ]
+            , div []
+                [ label []
+                    [ text "Description:" ]
+                ]
+            , div []
+                [ textarea [ onInput UpdateDescription ]
+                    []
+                ]
+            , div []
+                [ label []
+                    [ text "Time:" ]
+                ]
+            , div []
+                [ input [ type_ "date", onInput UpdateDate ]
+                    []
+                , input [ type_ "time", onInput UpdateFrom ]
+                    []
+                , input [ type_ "time", onInput UpdateTo ]
+                    []
+                ]
+            ]
+        ]
