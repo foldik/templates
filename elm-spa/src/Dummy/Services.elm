4ced9e7f952fe8d66ccdb8aa256e12b885263fd0
foldik <foldi.kristof@gmail.com>
2019-06-17 19:08:25 +0200

    Removed none existing services

--- elm-spa/src/Dummy/Services.elm
+++ elm-spa/src/Dummy/Services.elm
@@ -39,52 +39,7 @@ stateToString state =
 
 list : List Service
 list =
-    [ { name = "hello-world"
-      , version = "0.1.0"
-      , state = Starting
-      , instances = 2
-      , url = "http://localhost:9000/hello-world"
-      }
-    , { name = "users"
-      , version = "0.2.1"
-      , state = FailedToStart
-      , instances = 1
-      , url = "http://localhost:9000/users"
-      }
-    , { name = "tokens"
-      , version = "0.9.1"
-      , state = Running
-      , instances = 2
-      , url = "http://localhost:9000/tokens"
-      }
-    , { name = "git"
-      , version = "1.0.2"
-      , state = Running
-      , instances = 10
-      , url = "http://localhost:9000/git"
-      }
-    , { name = "drone"
-      , version = "0.8.2"
-      , state = Running
-      , instances = 11
-      , url = "http://localhost:9000/drone"
-      }
-    , { name = "teamcity"
-      , version = "2.1.6"
-      , state = Running
-      , instances = 7
-      , url = "http://localhost:9000/teamcity"
-      }
-    , { name = "jenkins"
-      , version = "2.1.1"
-      , state = Error
-      , instances = 1
-      , url = "http://localhost:9000/jenkins"
-      }
-    , { name = "new-meeting-service"
-      , version = "0.7.1"
-      , state = Running
-      , instances = 2
-      , url = "http://localhost:9000/meetings/new"
-      }
+    [ { name = "new-project", version = "0.1.0", state = Starting, instances = 1, url = "http://localhost:9000/projects/new" }
+    , { name = "new-user", version = "0.4.1", state = Running, instances = 2, url = "http://localhost:9000/users/new" }
+    , { name = "new-meeting", version = "0.7.1", state = Running, instances = 2, url = "http://localhost:9000/meetings/new" }
     ]
